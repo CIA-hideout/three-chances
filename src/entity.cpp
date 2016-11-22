4ce17191057b4f5f9bafade299b785dbe1acd721
Ivan Tan <ivantjh97@gmail.com>
2016-11-22 23:07:38 +0800

    Implement basic structure for entity class

---
+++ src/entity.cpp
@@ -0,0 +1,28 @@
+#include "entity.h"
+
+Entity::Entity() : Image() {}
+
+Entity::~Entity() {}
+
+bool Entity::initialize(Game *gamePtr, int width, int height, int ncols,
+	TextureManager *textureM, EntityData ed) {
+	health = ed.health;
+	damage = ed.damage;
+	atkRange = ed.atkRange;
+	moves = ed.moves;
+	levels = ed.levels;
+	abilities = ed.abilities;
+
+	// Initialize image
+	input = gamePtr->getInput();
+	return(Image::initialize(gamePtr->getGraphics(), width, height, ncols, textureM));
+}
+
+void Entity::update(float frameTime) {
+	// Call the base image update func to update the screen
+	Image::update(frameTime);
+}
+
+void Entity::ai(float frameTime, Entity &ent) {
+
+}
---
+++ src/entity.h
@@ -0,0 +1,61 @@
+#ifndef _ENTITY_H
+#define _ENTITY_H
+
+#include <vector>
+#include <string>
+#include "image.h"
+#include "game.h"
+
+namespace entityNS
+{
+	enum ABILITY_TYPE { TRAVEL_WATER, TRAVEL_LAVA, TRAVEL_WALL, BURNS_PLAYER, SHOOTS_FIRE };
+}
+
+struct EntityData {
+	float health;
+	float damage;
+	int atkRange;
+	int moves;
+	std::vector<int> levels;
+	std::vector<entityNS::ABILITY_TYPE> abilities;
+};
+
+class Entity: public Image {
+protected:
+	float health;	// there is 0.5 dmg
+	float damage;
+	int atkRange;
+	int moves;
+	std::vector<int> levels;
+	std::vector<entityNS::ABILITY_TYPE> abilities;
+
+	// variables needed for update
+	Input *input;
+
+public:
+	Entity();
+	~Entity();
+
+	// getters
+	float getHealth() const { return health; }
+	float getDamamge() const { return damage; }
+	int getAtkRange() const { return atkRange; }
+	int getMoves() const { return moves; }
+	std::vector<int> getLevels() const { return levels; }
+	std::vector<entityNS::ABILITY_TYPE> getAbilities() const { return abilities; }
+
+	// setters
+	void setHealth(float h) { health = h; }
+	void setDamage(float d) { damage = d; }
+	void setAtkRange(int atkR) { atkRange = atkR; }
+	void setMoves(int m) { moves = m; }
+	void setLevel(std::vector<int> l) { levels = l; }
+	void setAbilities(std::vector<entityNS::ABILITY_TYPE> a) { abilities = a; }
+
+	// can be overridden
+	virtual bool initialize(Game *gamePtr, int width, int height, int ncols,
+		TextureManager *textureM, EntityData ed);
+	virtual void update(float frameTime);
+	virtual void ai(float frameTime, Entity &ent);
+};
+#endif
